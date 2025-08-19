9a0a9dcc76b0b0ee41d91b6bdabea9d8a633137c
LathikaMBH <lathika.mbh@gmail.com>
2025-08-19 19:40:06 +0300

    how to assign variable to the middle of the statement. how to use specicifers

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	const name = "Lathika"
+	const age = 30
 	fmt.Println("Hello, Go! Welcome to my Go learning journey.😀")
+	fmt.Printf("My name is %s and I am %d years old.\n", name, age)
+	fmt.Printf("My name is %s and I am %d years old.\t data type of name is %T and data type of age is %T", name, age, name, age)
+
 }
